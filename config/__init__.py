5bba3157a367b3438e0ed1e7d392360490757707
MagnosLow <arturmagnonew@gmail.com>
2026-06-26 19:20:15 -0300

    cria __init__ na pasta config

--- config/__init__.py
+++ config/__init__.py
@@ -1,2 +1,2 @@
-from config.database import DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, LOGIN_USER, LOGIN_PASS
+from config.database import DB_HOST, DB_USER, DB_PASSWORD, DB_NAME
 from config.styles import BASE_DIR, ASSETS_DIR, COLORS, FONTS
